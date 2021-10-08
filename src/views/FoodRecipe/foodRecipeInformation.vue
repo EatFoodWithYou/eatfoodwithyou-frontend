21acec469c64db714d77a47440d60369d0c8da49
Thammasorn Yimsawat <thammasorn.y@ku.th>
2021-10-08 19:37:16 +0700

    add FoodRecipeInfor and sent params by routerlink

---
+++ src/views/FoodRecipe/foodRecipeInformation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Food Recipe ID : {{ this.id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+      return {
+        id:""
+      }
+    },
+    created(){
+      this.id = this.$route.params.id
+    }
+}
+</script>
+
+<style>
+
+</style>
